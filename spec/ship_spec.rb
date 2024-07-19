f6e170e1fb8a51b0f1b0032438b3368f5889d027
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-18 18:41:46 -0600

    test: can intialize ship object

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,5 +1,11 @@
 require './lib/ship'
 
 RSpec.describe Ship do
-    
+    before(:each) do
+        @cruiser = Ship.new("Cruiser", 3)
+    end
+
+    it 'can initialize' do
+        expect(@cruiser).to be_an_instance_of(Ship)
+    end
 end
