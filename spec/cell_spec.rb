a85ccade8062e435a41ba3f62658233eb0ad6824
Jim Macur <jim.macur@gmail.com>
2024-07-18 20:04:13 -0600

    test: place ship

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,6 +4,7 @@ require './lib/cell'
 RSpec.describe Cell do
     before(:each) do 
         @cell = Cell.new("B4")
+        @cruiser = Ship.new("Cruiser", 3)
     end
 
     describe '#initialize' do
@@ -23,6 +24,13 @@ RSpec.describe Cell do
 
         it 'is empty by default' do
             expect(@cell.empty?).to be true
+        end
+
+        it 'can place a ship' do
+            @cell.place_ship(@cruiser)
+
+            expect(@cell.ship).to eq @cruiser
+            expect(@cell.empty?).to be false
         end
     end
 
