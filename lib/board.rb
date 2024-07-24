da9612ddad482aab98803089ef9609e17fc99662
Jim Macur <jim.macur@gmail.com>
2024-07-23 19:25:36 -0600

    refactor: indents, delete psuedocode, delete extra files

--- lib/board.rb
+++ lib/board.rb
@@ -45,15 +45,15 @@ class Board
             number_coord << coordinate[1].to_i
         end
         
-       if letter_coord.all?(letter_coord[0])
+        if letter_coord.all?(letter_coord[0])
             (1..4).each_cons(ship.length) { |sub_array| valid_arrays << sub_array }
             valid_arrays.include?(number_coord) && coordinates.length == ship.length
             
-       elsif number_coord.all?(number_coord[1])
+        elsif number_coord.all?(number_coord[1])
             ("A".."D").each_cons(ship.length) { |sub_array| valid_arrays << sub_array } 
             valid_arrays.include?(letter_coord) && coordinates.length == ship.length
 
-       end
+        end
     end
 
     def available?(ship, coordinates)
@@ -63,25 +63,25 @@ class Board
     end
 
     def place(ship, coordinates)
-         coordinates.each do |coordinate|
+        coordinates.each do |coordinate|
             cells[coordinate].place_ship(ship)
-         end       
+        end       
     end
 
     def render(player_board = false)
 
         if player_board == false
             board  = "  1 2 3 4 \n" +
-                 "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render}\n" +
-                 "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render}\n" +
-                 "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render}\n" +
-                 "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render}\n"
+                "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render}\n" +
+                "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render}\n" +
+                "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render}\n" +
+                "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render}\n"
         else
             board  = "  1 2 3 4 \n" +
-                 "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)}\n" +
-                 "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)}\n" +
-                 "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)}\n" +
-                 "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)}\n"
+                "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)}\n" +
+                "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)}\n" +
+                "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)}\n" +
+                "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)}\n"
         end
     end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,30 +6,9 @@ require './lib/game'
 RSpec.describe Game do
     before(:each) do
         @game = Game.new
-        @player_board = Board.new
-        @computer_board = Board.new
-        @cruiser = Ship.new("Cruiser", 3)
-        @submarine = Ship.new("Submarine", 2)
-        @rubber_duckie = Ship.new("Duckie", 1)
     end
 
     it 'exists' do
         expect(@game).to be_an_instance_of Game
     end
-
-    describe '#start' do
-        xit 'puts welcome message' do
-            expect(@game.welcome).to be_an_instance_of String
-        end
-
-    end
-
-    it 'places computer ships' do
-        # @game.computer_placement
-        @computer_board.place(@cruiser, ["A1", "A2", "A3"])
-        # require 'pry'; binding.pry
-
-        expect(@computer_board.cells[:ship]).to include(Ship)
-    end
-
 end
